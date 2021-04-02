9f342abc254f12e2804fc0f37f5a3538529d6d77
Ana <anaurrig@gmail.com>
2021-04-02 12:35:34 +0200

    feat:login con passport

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -18,18 +18,18 @@ const passport = require('passport');
 
   
   module.exports.login = (req, res, next) => {
-    // passport.authenticate('local-auth', (error, user, validations) => {
-    //   if (error) {
-    //     next(error);
-    //   } else if (!user) {
-    //     next(createError(400, { errors: validations }))
-    //   } else {
-    //     req.login(user, error => {
-    //       if (error) next(error)
-    //       else res.json(user)
-    //     })
-    //   }
-    // })(req, res, next);
+    passport.authenticate('local-auth', (error, user, validations) => {
+      if (error) {
+        next(error);
+      } else if (!user) {
+        next(createError(400, { errors: validations }))
+      } else {
+        req.login(user, error => {
+          if (error) next(error)
+          else res.json(user)
+        })
+      }
+    })(req, res, next);
   };
 
   module.exports.logout = (req, res, next) => {
